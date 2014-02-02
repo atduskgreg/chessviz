bd2de08d794e5ac5d71d93e4fa2d2e4c881839c5
Greg Borenstein <greg.borenstein@gmail.com>
2014-02-02 15:49:00 -0500

    add ability to load fen from url param

--- app.rb
+++ app.rb
@@ -5,7 +5,8 @@ require 'json'
 UCI = Uci.new( :engine_path => '/usr/local/bin/stockfish' )
 
 get "/" do
-	send_file File.expand_path('index.html', settings.public_folder)
+	@fenstring = params[:f]
+	erb :index
 end
 
 get "/position" do
